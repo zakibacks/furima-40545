4843d10a88794ac01c2ae2c67d471aa8ac2931ac
zakibacks <165463445+zakibacks@users.noreply.github.com>
2024-04-30 21:56:26 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :authenticate_user!, only: [:show]
   before_action :set_tweet, only: [:edit, :show, :update]
 
   def index
