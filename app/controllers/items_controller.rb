a3bbaaaf75f6b0cc1cb63a29cc116440ed3362a0
zakibacks <165463445+zakibacks@users.noreply.github.com>
2024-04-30 21:46:45 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_tweet, only: [:edit, :show]
+  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :set_tweet, only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,14 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to root_path, alert: "You are not authorized to edit this item."
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
