86ff76c46c71cbcf51afe0f5008a132f6b636864
zakibacks <165463445+zakibacks@users.noreply.github.com>
2024-04-30 14:31:55 +0900

    修正４

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,16 @@
-const priceInput = document.getElementById("item-price");
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
+
   const addTaxDom = document.getElementById("add-tax-price");
   const commission = Math.floor(inputValue * 0.1);
   addTaxDom.innerHTML =  commission ;
+
   const profitDom = document.getElementById("profit");
+  const benefit = Math.floor(inputValue * 0.1);
   const profit = inputValue - commission;
   profitDom.innerHTML =  profit ;
 })
+});
+  
