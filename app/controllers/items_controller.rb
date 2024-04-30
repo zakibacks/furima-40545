90a2060da40380f44db06987556aaaa8bf1c34be
zakibacks <165463445+zakibacks@users.noreply.github.com>
2024-04-30 16:34:19 +0900

    商品一覧表示機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    # @items = Item.order('created_at DESC')
+    @items = Item.order('created_at DESC')
+    # @items = Item.all
   end
 
   def new
