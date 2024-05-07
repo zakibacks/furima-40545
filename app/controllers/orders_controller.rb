2357afeb9ed616de7ebd9d60dabea78ce9f61d6d
zakibacks <165463445+zakibacks@users.noreply.github.com>
2024-05-07 18:49:55 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
 
   def non_purchased_item
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.orders.present?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
   def order_delivery_params
